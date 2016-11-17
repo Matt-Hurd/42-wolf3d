1ecc11949310d7e0f45afb966a1b645deaa5f7ad
Matt-Hurd <sabahbob@gmail.com>
2016-11-16 18:14:23 -0800

    Updated Usage and controls

--- src/main.c
+++ src/main.c
@@ -6,15 +6,20 @@
 /*   By: mhurd <mhurd@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/08 15:35:03 by mhurd             #+#    #+#             */
-/*   Updated: 2016/10/15 13:50:09 by mhurd            ###   ########.fr       */
+/*   Updated: 2016/11/15 15:20:10 by mhurd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-#include <stdio.h>
+void	display_controls(void)
+{
+	ft_putendl("CONTROLS:\n\
+Movement:\n\
+	Arrow Keys");
+}
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	t_data	*data;
 	t_wolf	*wolf;
@@ -27,7 +32,14 @@ int	main(int ac, char **av)
 			ft_error("Malloc Error");
 		data->w = wolf;
 		wolf->plot = parse_file(av[1]);
+		display_controls();
 		draw_everything(data, wolf);
+	}
+	else
+	{
+		ft_putstr("Usage: ");
+		ft_putstr(av[0]);
+		ft_putendl(" [map_name]");
 	}
 	return (0);
 }
